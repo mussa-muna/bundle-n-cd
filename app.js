2486f34be2c5224a6d8d8229c2c1ee8fd7f3068c
Muna Mussa <munammahdi@gmail.com>
2023-10-14 00:18:49 +0300

    add a new route

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
+// Add the "/name" route
+app.get('/name/:name', (req, res) => {
+    const { name } = req.params;
+    res.send(`Hello, ${name}!`);
+  });
+  
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
