b318ba40136249cfb6ace2e5de56360cb3d98c6e
Muna Mussa <munammahdi@gmail.com>
2023-10-15 04:16:20 +0300

    bundle n test video

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
 const port = 80;
-// hello
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
-
+// Adding tests
 app.get("/test", (req, res) => {
   res.send("Test Route!");
 });
